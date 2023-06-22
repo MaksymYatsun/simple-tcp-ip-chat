9ceecc6f9e1c2b9111e34793e06ac11d7567ba6b
MaksymYatsun <egocogitio3.14@gmail.com>
2023-06-22 17:58:22 +0300

    Added json functionality for name or message receiving. Small refactoring.

--- chat.js
+++ chat.js
@@ -1,38 +1,47 @@
-const stdin = process.openStdin();
 const net = require('net');
+const stdin = process.openStdin();
 
 const [username, port, receiverPort] = process.argv.slice(2)
 
 const hostname = 'localhost';
+let friendsName = null;
 
 const server = net.createServer((socket) => {
   socket.on('data', data => {
-    console.log(data.toString());
+    const received = JSON.parse(data.toString())
+    if (received.message) {
+      console.log(`${friendsName}: ${received.message}`)
+    }
+    if (received.name) {
+      friendsName = received.name;
+      console.log(`${friendsName} has entered a chat room`)
+    }
   });
 
   socket.on('error', () => {
-    console.log('Your friend has left the chat room')
+    console.log(`${friendsName} has left the chat room`)
   });
 });
 
 server.listen(port, hostname, () => {
   console.log(`Accepting connections on ${hostname}:${port}`);
+});
 
-  const socket = new net.Socket();
+const socket = new net.Socket();
 
-  socket.on('error', () => {
-    console.log('Connection to a friend was refused retrying in 5s');
-    setTimeout(() => {
-      socket.connect(receiverPort);
-    }, 5000);
-  });
+socket.on('error', () => {
+  console.log('Connection to a friend was refused retrying in 5s');
+  setTimeout(() => {
+    socket.connect(receiverPort);
+  }, 5000);
+});
 
-  socket.connect(receiverPort, () => {
-    console.log(`Connected to a friend ${hostname}:${receiverPort}`);
-    socket.write(`${username} has entered the chat room`);
-  });
+socket.connect(receiverPort, () => {
+  const message = JSON.stringify({ name: username })
+  console.log(`Connected to a friend at ${hostname}:${receiverPort}`);
+  socket.write(message)
+});
 
-  stdin.addListener("data", d => {
-    socket.write(`${username}: ${d}`);
-  });
+stdin.addListener("data", d => {
+  socket.write(JSON.stringify({ message: d.toString() }));
 });
